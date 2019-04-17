c4d16b5e4e8dd26cf10ce5f100e08f97e71effd4
kasukkutti <suresh@mookambikainfo.com>
2019-04-17 13:06:33 +0530

    Add files via upload

---
+++ src/app/confirmation-dialog-leavepage/confirmation-dialog-leavepage.component.ts
@@ -0,0 +1,13 @@
+import { Component, Inject, ViewChild, ElementRef } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+    selector: 'confirmation-dialog-leavepage',
+    templateUrl: 'confirmation-dialog-leavepage.html',
+    styleUrls: ['./styles.styl'],
+})
+
+export class ConfirmationDialogLeavePage {
+    constructor(public dialogRef: MatDialogRef<ConfirmationDialogLeavePage>, @Inject(MAT_DIALOG_DATA) private data: any) {        
+    }
+}
